cd8b049b2442d380ae7486e2113724330947ada2
MariiaHula <mariyagulaya@gmail.com>
2023-11-13 13:05:54 +0200

    final changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import CategoryLoader from './ContentLoader';
 import Layout from './Layout';
 import Home from '../pages/Home';
 import Phonebook from '../pages/Phonebook';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import NotFound from 'pages/NotFound';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { refreshUserThunk } from 'redux/auth/operations';
 import PrivateRoute from './hoc/PrivateRoute';
 import { selectIsRefresh } from 'redux/auth/selectors';
-import CategoryLoader from './ContentLoader';
 
 export const App = () => {
   const dispatch = useDispatch();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import img_2 from '../images/img_2.jpg';
-import { PhoneIcon } from '@heroicons/react/24/solid';
 
 const Home = () => {
   return (
@@ -9,28 +8,29 @@ const Home = () => {
       className="bg-cover bg-center min-h-screen flex items-center justify-center flex-wrap"
       style={{ backgroundImage: `url(${img_2})` }}
     >
-      <div className="text-center max-w-[220px] px-4 flex-col items-center rotate-1 mt-36 mb-10  md:mt-40 md:justify-center md:min-w-[220px] lg:mt-50  lg:min-w-[240px] xl:min-w-[280px]">
-        <h1 className=" text-3xl mb-12 md:text-[32px]  lg:text-4xl  font-bold xl:text-[44px]  ">
+      <div className="text-center max-w-[220px] px-4 flex-col items-center rotate-1 mt-40 mb-4 md:px-2 md:justify-center md:min-w-[260px] xl:min-w-[280px]">
+        <h1 className=" text-4xl mb-12  lg:text-4xl  font-bold xl:text-[44px]  ">
           Welcome to Phonebook!
         </h1>
 
-        <Link
-          className="btn btn-outline btn-info text-lg md:text-xl md:mb-3 lg:text-2xl lg:mb-4 xl:text-3xl xl:mb-6 "
-          to="/login"
-        >
-          Login
-        </Link>
-        <Link
-          className="btn btn-outline btn-warning text-lg md:text-xl lg:text-2xl mt-2 md:mt-0 lg:mb-4 xl:text-3xl "
-          to="/register"
-        >
-          Register
-        </Link>
+        <div className="flex flex-col items-center justify-center">
+          <Link
+            className="btn btn-outline btn-info text-lg md:text-xl md:mb-3  lg:text-2xl lg:mb-4 xl:text-3xl xl:mb-6"
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="btn btn-outline btn-warning text-lg md:text-xl lg:text-2xl mt-2 md:mt-0 lg:mb-4 xl:text-3xl"
+            to="/register"
+          >
+            Register
+          </Link>
+        </div>
 
-        <p className="mt-6 mb-12 text-center md:text-lg lg:text-xl font-bold whitespace-normal xl:text-2xl xl:mb-26">
+        <p className="mt-6 mb-36 text-center md:text-lg lg:text-xl font-bold whitespace-normal xl:text-2xl xl:mb-26">
           Easy to search & quick to save!
         </p>
-        <PhoneIcon className="h-8 w-8 md:h-10 md:w-10 text-sky-600 m-0 auto" />
       </div>
     </div>
   );
